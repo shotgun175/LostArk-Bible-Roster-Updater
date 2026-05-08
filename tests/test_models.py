ce342d7f893f14ad868001195092497c9c54ad95
shotgun175 <bharatvalluru@gmail.com>
2026-05-08 10:25:39 -0400

    chore: gitignore hygiene + remove fluff

--- tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from models import Character
-
-
-def test_character_fields():
-    char = Character(name="Valslayer", ilvl=1755, cp=5915.7, char_class="Slayer")
-    assert char.name == "Valslayer"
-    assert char.ilvl == 1755
-    assert char.cp == 5915.7
-    assert char.char_class == "Slayer"
